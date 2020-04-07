8484f077a14e6074bb201d571ab75f9b5267b704
Your Name <you@example.com>
2020-04-07 21:37:13 +0200

    Initial commit

---
+++ Excersises.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Excersises {
+
+    public static void main (String args[]){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input first number: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Input second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + " x " + num2 + " = " + num1*num2);
+        
+    }
+}
