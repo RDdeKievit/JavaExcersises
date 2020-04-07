4eb331666cc944aedde749c3cd69d69dd0102a9c
Your Name <you@example.com>
2020-04-07 22:28:24 +0200

    Basic Part-I-6

--- Excersises.java
+++ Excersises.java
@@ -12,7 +12,11 @@ class Excersises {
         System.out.println("Input second number: ");
         int num2 = input.nextInt();
 
+        System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
         System.out.println(num1 + " x " + num2 + " = " + num1*num2);
+        System.out.println(num1 + " / " + num2 + " = " + num1/num2);
+        System.out.println(num1 + " mod " + num2 + " = " + num1%num2);
         
     }
 }
