3de496cfca6fb6d87629fd64f1dd5e2d4f816370
Your Name <you@example.com>
2020-04-10 17:45:04 +0200

    Excersize31

--- Excersises.java
+++ Excersises.java
@@ -2,20 +2,12 @@ import java.util.Scanner;
 
 class Excersises {
 
-    public static void main (String args[]){
-
-        int a, b, temp;
-
-        a = 3;
-        b = 14;
-
-        System.out.println("Before swap: a, b = "+ a +","+ b);
-
-        temp = a;
-        a = b;
-        b = temp;
-
-        System.out.println("After swap: a, b = "+ a +","+ b);
-
-    }
+public static void main(String[] args) {
+    System.out.println("\nJava Version: "+System.getProperty("java.version"));
+    System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
+    System.out.println("Java Home: "+System.getProperty("java.home"));
+    System.out.println("Java Vendor: "+System.getProperty("java.vendor"));
+    System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));
+    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");
+ }
 }
